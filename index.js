2b9cee6ea8d76284fb881b643b0bff3a27a6dc20
hasib1511 <hasib7143@gmail.com>
2023-05-18 05:39:21 +0600

    added post method

--- index.js
+++ index.js
@@ -27,6 +27,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db("toyMarketplace");
+    const toysCollection = db.collection("toys");
+// post a toy
+    app.post("/toys", async (req, res) => {
+      const body = req.body;
+      
+      const result = await toysCollection.insertOne(body);
+      
+      
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
